54ce07ebba4fbfbb9d48104003da703568088fcf
Satyaranjan38 <146444908+Satyaranjan38@users.noreply.github.com>
2024-10-17 18:06:53 +0530

    Update script.js: Added new log statement.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-// console.log("Hello world")
-// console.log("Hello world1")
-console.log("Hello world2")
-console.log('New log added');
+console.log("Hello world")
+console.log("Hello world1")
+// console.log("Hello world2")
+// console.log('New log added');
 console.log("Hello world3")
 console.log("Hello world4")
